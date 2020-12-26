be2fb8246633611ad36a1cd6e8c10e4d6206f852
Vlad Comanescu <vladcomanescu@yahoo.com>
2020-12-26 18:37:45 +0000

    Separate component from main app

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, KeyboardAvoidingView, Platform } from 'react-native';
+import SearchInput from './components/SearchInput';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+        <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <Text style={[styles.largeText, styles.textStyle]}>Brasilia</Text>
+        <Text style={[styles.smallText, styles.textStyle]}>Nubes y borasca</Text>
+        <Text style={[styles.largeText, styles.textStyle]}>20°</Text>
+        <SearchInput placeholder="Search any city" />
+        </KeyboardAvoidingView>
+    );
+  }
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +23,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    textAlign: 'center',
+    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+  },
+  largeText: {
+    fontSize: 44,
+  },
+  smallText: {
+    fontSize: 18,
+  }
 });
