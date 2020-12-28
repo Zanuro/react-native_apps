147fb99904053854fab5552aa640e71445ea9272
Vlad Comanescu <vladcomanescu@yahoo.com>
2020-12-28 20:39:34 +0000

    Creating some basic layout and components for the tracker app

---
+++ timer_app/components/TimerButton.js
@@ -0,0 +1,42 @@
+import react from 'react';
+import {StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
+
+
+export default function TimerButton({ color, title, small, onPress}){
+
+    return (
+        <TouchableOpacity style={[styles.button, {borderColor: color}]}
+        onPress={onPress}>
+            <Text style={[styles.buttonText, small ? styles.small : styles.large, { color}]}> {title}</Text>
+
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    button:{
+        marginTop: 12,
+        minWidth: 100,
+        borderWidth: 2,
+        borderRadius: 3,
+    },
+    small: {
+        fontSize: 14,
+        padding: 5,
+    },
+    large: {
+        fontSize: 16,
+        padding: 10,
+    },
+    buttonText: {
+        textAlign: 'center',
+        fontSize: 10,
+        fontWeight: 'bold',
+    },
+    title:{
+        fontSize: 14,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        paddingVertical: 10,
+    },
+})
